104a67ebe4a0b7ec5a2f2016f44bde29d99ec925
Sercan Altundas <sercan.altundas@gmail.com>
2020-07-22 21:59:30 +0300

    updates

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,22 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { getScreenshot } from './_lib/chromium';
+import { launch } from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
     console.log('Current Platform:' + process.platform)
     console.log(req)
-    const file = await getScreenshot();
+    
+    const browser = await launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 2048, height: 1170 });
+    await page.goto('https://www.facebook.com');
+    const file = await page.screenshot({ type: 'jpeg' });
+
     res.statusCode = 200;
     res.setHeader('Content-Type', `image/jpeg`);
     res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
