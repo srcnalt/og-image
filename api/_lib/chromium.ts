91abc10389e7b89c54679f25b0bb62becde12ab7
Sercan <srcnalt@users.noreply.github.com>
2020-07-22 18:13:23 +0300

    Update chromium.ts

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,8 +1,7 @@
 import { launch } from 'puppeteer-core';
 import chrome from 'chrome-aws-lambda';
-import { FileType } from './types';
 
-export async function getScreenshot(type: FileType) {
+export async function getScreenshot() {
     const browser = await launch({
         args: chrome.args,
         executablePath: await chrome.executablePath,
@@ -12,6 +11,6 @@ export async function getScreenshot(type: FileType) {
     const page = await browser.newPage();
     await page.setViewport({ width: 2048, height: 1170 });
     await page.goto('https://www.facebook.com');
-    const file = await page.screenshot({ type });
+    const file = await page.screenshot({ type: 'jpeg' });
     return file;
 }
