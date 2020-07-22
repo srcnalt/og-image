64d12ee8bdc38c5751397d80bbd397228b988f3b
Sercan <srcnalt@users.noreply.github.com>
2020-07-22 17:51:17 +0300

    update

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,26 +1,17 @@
-import { launch, Page } from 'puppeteer-core';
+import { launch } from 'puppeteer-core';
 import chrome from 'chrome-aws-lambda';
 import { FileType } from './types';
-let _page: Page | null;
 
-async function getPage() {
-    if (_page) {
-        return _page;
-    }
-
+export async function getScreenshot(type: FileType) {
     const browser = await launch({
         args: chrome.args,
         executablePath: await chrome.executablePath,
         headless: chrome.headless,
     });
-    _page = await browser.newPage();
-    return _page;
-}
 
-export async function getScreenshot(html: string, type: FileType) {
-    const page = await getPage();
+    const page = await browser.newPage();
     await page.setViewport({ width: 2048, height: 1170 });
-    await page.setContent(html);
+    await page.goto('www.google.com');
     const file = await page.screenshot({ type });
     return file;
 }
--- api/index.ts
+++ api/index.ts
@@ -17,7 +17,7 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
             return;
         }
         const { fileType } = parsedReq;
-        const file = await getScreenshot(html, fileType);
+        const file = await getScreenshot(fileType);
         res.statusCode = 200;
         res.setHeader('Content-Type', `image/${fileType}`);
         res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
